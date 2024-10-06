bea39d8a1ac1f237415d0eb39d57167a64106b52
brochua <brochua17@gmail.com>
2024-10-06 18:16:04 -0400

    Feat: Create skeleton Home component with tests

---
+++ porogo/frontend/src/components/__tests__/Home.test.js
@@ -0,0 +1,20 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen, cleanup } from '@testing-library/react';
+import Home from '../Home.tsx';
+
+afterEach(cleanup)
+
+describe('Rendering home', () => {
+	it('Renders header text', () => {
+		render(<Home />);
+		
+		const home = screen.findByRole('heading', { name: /home/ });
+		
+		expect(home).toBeInTheDocument();
+	});
+});
