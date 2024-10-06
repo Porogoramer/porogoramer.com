2f813f5a8a311128b0f5a81e86b4851946ce2a8a
brochua <brochua17@gmail.com>
2024-10-06 18:32:59 -0400

    Feat: Create skeleton component Logs with tests

---
+++ porogo/frontend/src/components/__tests__/Logs.test.js
@@ -0,0 +1,20 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen, cleanup } from '@testing-library/react';
+import Logs from '../Logs.tsx';
+
+afterEach(cleanup)
+
+describe('Rendering logs', () => {
+	it('Renders header text', () => {
+		render(<Logs />);
+		
+		const logs = screen.findByRole('heading', { name: /logs/i });
+		
+		expect(logs).toBeInTheDocument();
+	});
+});
