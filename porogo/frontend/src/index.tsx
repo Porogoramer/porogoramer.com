14a976945e574100b767cbacf373c9cfc67469c8
Brochua <brochua17@gmail.com>
2024-10-06 20:53:45 -0400

    Feat: Create basic react route

--- porogo/frontend/src/index.tsx
+++ porogo/frontend/src/index.tsx
@@ -1,8 +1,40 @@
 import React from 'react';
-import App from './components/App';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import About from './components/views/About';
+import Contact from './components/views/Contact';
+import Dashboard from './components/views/Dashboard';
+import Home from './components/views/Home';
+import Projects from './components/views/Projects';
 
 const appDiv = document.getElementById('app')!;
 const root = createRoot(appDiv);
 
-root.render(<App />);
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />,
+    },
+    {
+        path: '/about',
+        element: <About />,
+    },
+    {
+        path: '/projects',
+        element: <Projects />,
+    },
+    {
+        path: '/contact',
+        element: <Contact />,
+    },
+    {
+        path: '/dashboard',
+        element: <Dashboard />,
+    },
+]);
+
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
