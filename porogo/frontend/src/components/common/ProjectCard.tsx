badc13a23f21906d51a90d22acc043e6067fd3cc
Brochua <brochua17@gmail.com>
2024-10-08 18:47:23 -0400

    Feat: Create basic component for project cards

---
+++ porogo/frontend/src/components/common/ProjectCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ProjectCard() {
+    return <article>
+        <h2>Project Name</h2>
+        <p>Project Desc</p>
+        <button>Learn more (to project detiails)</button>
+        <a href="#"><img src="/static/logos/github.svg" alt="Github Logo" /></a>
+    </article>;
+}
