42ec3fd8b0601e101a564366c542531ebb1a3f49
Brochua <brochua17@gmail.com>
2024-10-07 20:16:59 -0400

    Setup: Add general structure of Home component

--- porogo/frontend/src/components/views/Home.tsx
+++ porogo/frontend/src/components/views/Home.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import Header from '../common/Header';
+import Footer from '../common/Footer';
 
 export default function Home() {
-    return <h1>Home</h1>;
+    return <>
+        <Header />
+        <section className='content'>
+            <h1>Hi, we are Porogo</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam accusamus quos quidem veniam sapiente corporis? Quidem porro fuga dolor amet soluta delectus atque aliquam obcaecati, vel consequuntur reprehenderit earum neque.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam accusamus quos quidem veniam sapiente corporis? Quidem porro fuga dolor amet soluta delectus atque aliquam obcaecati, vel consequuntur reprehenderit earum neque.</p>
+            <img src="/static/assets/logos/porogoIcon.svg" alt="Porogo Logo" />
+        </section>
+        <section className='content'>
+            <div>
+                <h1>Projects</h1>
+                <div>Proj1</div>
+                <div>Proj2</div>
+            </div>
+        </section>
+        <Footer />
+    </>;
 }
