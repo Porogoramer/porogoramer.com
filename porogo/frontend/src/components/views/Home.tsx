e871e45d460c02859ec69d775afcf765ed388b01
Brochua <113867233+Brochua@users.noreply.github.com>
2024-10-08 17:03:24 -0400

    Style: Implement welcome section layout

--- porogo/frontend/src/components/views/Home.tsx
+++ porogo/frontend/src/components/views/Home.tsx
@@ -5,11 +5,11 @@ import Footer from '../common/Footer';
 export default function Home() {
     return <>
         <Header />
-        <section className='content'>
-            <div>
-                <h1>Hi, we are Porogo</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam accusamus quos quidem veniam sapiente corporis? Quidem porro fuga dolor amet soluta delectus atque aliquam obcaecati, vel consequuntur reprehenderit earum neque.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam accusamus quos quidem veniam sapiente corporis? Quidem porro fuga dolor amet soluta delectus atque aliquam obcaecati, vel consequuntur reprehenderit earum neque.</p>
+        <section className='content landing'>
+            <div className='welcome'>
+                <h1>Welcome to Porogo</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam accusamus quos quidem veniam sapiente corporis? Quidem porro fuga dolor amet soluta delectus atque aliquam obcaecati, vel consequuntur reprehenderit earum neque.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam accusamus quos quidem veniam sapiente corporis? Quidem porro fuga dolor amet soluta delectus atque aliquam obcaecati, vel consequuntur reprehenderit earum neque.</p>
             </div>
             <img src="/static/assets/logos/porogoIcon.svg" alt="Porogo Logo" />
         </section>
