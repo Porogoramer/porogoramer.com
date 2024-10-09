b6a7381453b32ccee187e54d883a84812021177e
Brochua <brochua17@gmail.com>
2024-10-08 21:43:39 -0400

    Feat: Add Project cards to bottom of home page

--- porogo/frontend/src/components/common/ProjectCard.tsx
+++ porogo/frontend/src/components/common/ProjectCard.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function ProjectCard({ name, desc }: { name: string, desc: string, }) {
-    return <article>
-        <h2>{name}</h2>
-        <p>{desc}</p>
-        <Link to={`projects/${name}`}>
-            <button>Learn more!</button>
-        </Link>
-        <a href="github.com"><img src="/static/logos/github.svg" alt="Github Logo" /></a>
+    return <article className='project-card'>
+        <img src="/static/assets/logos/porogoIcon.svg" alt="placeholder" />
+        <div className='project-card-info'>
+            <h2>{name}</h2>
+            <p>{desc}</p>
+            <div>
+                <Link to={`projects/${name}`}>
+                    <button>Learn more!</button>
+                </Link>
+                <a href="github.com"><img src="/static/logos/github.svg" alt="Github Logo" /></a>
+            </div>
+        </div>
     </article>;
 }
--- porogo/frontend/src/components/views/Home.tsx
+++ porogo/frontend/src/components/views/Home.tsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import Header from '../common/Header';
 import Footer from '../common/Footer';
+import ProjectCard from '../common/ProjectCard';
 
 export default function Home() {
     return <>
         <Header />
-        <section className='content landing'>
-            <div className='welcome'>
-                <h1>Welcome to Porogo</h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam accusamus quos quidem veniam sapiente corporis? Quidem porro fuga dolor amet soluta delectus atque aliquam obcaecati, vel consequuntur reprehenderit earum neque.
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam accusamus quos quidem veniam sapiente corporis? Quidem porro fuga dolor amet soluta delectus atque aliquam obcaecati, vel consequuntur reprehenderit earum neque.
-                </p>
+        <section className='content'>
+            <div id='landing'>
+                <div className='welcome'>
+                    <h1>Welcome to Porogo</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam accusamus quos quidem veniam sapiente corporis? Quidem porro fuga dolor amet soluta delectus atque aliquam obcaecati, vel consequuntur reprehenderit earum neque.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam accusamus quos quidem veniam sapiente corporis? Quidem porro fuga dolor amet soluta delectus atque aliquam obcaecati, vel consequuntur reprehenderit earum neque.
+                    </p>
+                </div>
+                <img src="/static/assets/logos/porogoIcon.svg" alt="Porogo Logo" />
             </div>
-            <img src="/static/assets/logos/porogoIcon.svg" alt="Porogo Logo" />
         </section>
         <section className='content'>
-            <div>
+            <div id='home-projects'>
                 <h1>Projects</h1>
-                <div>Proj1</div>
-                <div>Proj2</div>
+                <ProjectCard name='Important Dog' desc='A discord bot that exists I guess' />
+                <ProjectCard name='Portfolio Site' desc='A site to showcase some projects and skills' />
             </div>
         </section>
         <Footer />
