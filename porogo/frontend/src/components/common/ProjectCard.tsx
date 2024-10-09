396fb6ffcea3f57a6220a64fcbbe1e3fcc503992
Brochua <brochua17@gmail.com>
2024-10-08 21:15:55 -0400

    Setup: Create ProjectCard component

--- porogo/frontend/src/components/common/ProjectCard.tsx
+++ porogo/frontend/src/components/common/ProjectCard.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function ProjectCard() {
+export default function ProjectCard({ name, desc }: { name: string, desc: string, }) {
     return <article>
-        <h2>Project Name</h2>
-        <p>Project Desc</p>
-        <button>Learn more (to project detiails)</button>
-        <a href="#"><img src="/static/logos/github.svg" alt="Github Logo" /></a>
+        <h2>{name}</h2>
+        <p>{desc}</p>
+        <Link to={`projects/${name}`}>
+            <button>Learn more!</button>
+        </Link>
+        <a href="github.com"><img src="/static/logos/github.svg" alt="Github Logo" /></a>
     </article>;
 }
