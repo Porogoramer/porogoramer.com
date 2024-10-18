29a68681c2894267a60e5bb5a9575e8ec2063127
Rida <ridachaarani@yahoo.com>
2024-10-18 17:10:31 -0400

    Feat: Made carousel have fade effect and added pagination dots

--- porogo/frontend/src/components/views/Projects.tsx
+++ porogo/frontend/src/components/views/Projects.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Swiper, SwiperSlide} from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
+import { Navigation, Pagination, A11y, Autoplay, EffectFade } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/bundle';
 import ProjectCard from '../common/ProjectCard';
@@ -8,26 +8,26 @@ import ProjectCard from '../common/ProjectCard';
 
 export default function Projects() {
     return <>
-    <section className='test'>
-        <section className='top-content'>
-            <Swiper className='carousel'
-            modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-            spaceBetween={50}
-            slidesPerView={1}
-            loop={true}
-            autoplay={{
-                delay: 10000,
-                disableOnInteraction: true,
-              }}
-            >
-                <SwiperSlide className='card'><ProjectCard name='Important Dog' desc='A discord bot that exists I guess' /></SwiperSlide>
-                <SwiperSlide className='card'><ProjectCard name='Portfolio Site' desc='A site to showcase some projects' /></SwiperSlide>
-            </Swiper>
-        </section>
+        <section className='test'>
+            <section className='top-content'>
+                <Swiper className='carousel'
+                    modules={[Navigation, Pagination, A11y, Autoplay, EffectFade]}
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    loop={true}
+                    autoplay={{
+                        delay: 5000,
+                        disableOnInteraction: true,
+                    }}
+                    pagination = {true}
+                    effect='fade'>
+                    <SwiperSlide className='card'><ProjectCard name='Important Dog' desc='A discord bot that exists I guess' /></SwiperSlide>
+                    <SwiperSlide className='card'><ProjectCard name='Portfolio Site' desc='A site to showcase some projects' /></SwiperSlide>
+                </Swiper>
+            </section>
         </section>
         <section className='full-content'>
-            
+
         </section>
-        </>
-    
+    </>;
 }
