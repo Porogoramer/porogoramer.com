4c21f0b44778b425f71af71ca17685f0c0168c70
Axel Brochu <113867233+Brochua@users.noreply.github.com>
2024-10-17 23:12:57 -0400

    Feat: Implement page home at / (#31)
    * Setup: Add general structure of Home component
    
    * Setup: Create scss files for homepage
    
    * Refactor: Update file structure
    
    * Update: Add main.css to gitignore file
    
    * Fix: Update webpack config to reload scss files on save
    
    * Style: Implement welcome section layout
    
    * Feat: Create script to run server and compile frontend at once
    
    * Refactor: Remove redundant imports in index.html and fix codestyle issues
    
    * Style: Add shading to the welcome screen and changed text color
    
    * Feat: Create basic component for project cards
    
    * Setup: Create ProjectCard component
    
    * Feat: Add Project cards to bottom of home page
    
    * Style: Adjust style for project cards
    
    * Style: Add hover and style button
    
    * Style: Adjust home page style for projects overview
    
    * Feat: Complete homepage
    
    Adjust font weight for Projects section of home page, adjust spacing between project cards, Fix favicon
    
    * Test: Add tests for homepage rendering, and links
    
    * refactor : update home page
    
    Update home page layout to better match Figma design.
    
    * refactor: update project card component
    
    Update project card component to match more Figma template.
    
    ---------
    
    Co-authored-by: Yan2arb4 <yan2arb6@hotmail.com>

---
+++ porogo/frontend/src/components/common/ProjectCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function ProjectCard({ name, desc }: { name: string, desc: string, }) {
+    return <article className='project-card'>
+        <img src="/static/assets/icons/porogoIcon.svg" alt="placeholder" />
+        <div className='project-card-info'>
+            <h2>{name}</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero voluptatibus harum velit facilis eligendi. Iusto delectus incidunt quae rem, voluptatum corrupti at autem molestiae, assumenda unde doloribus id totam mollitia.</p>
+            <div className='options'>
+                <Link to={`project/${name}`}>
+                    <button>Learn more!</button>
+                </Link>
+                <a className='github' target='__blank' href="https://github.com"><img src="/static/assets/icons/github-logo-black.svg" alt="Github Logo"/></a>
+            </div>
+        </div>
+    </article>;
+}
