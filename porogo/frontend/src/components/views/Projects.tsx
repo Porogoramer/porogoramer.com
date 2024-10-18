6c9fe041619827df5c9d080f94dbec5819f7daa5
Rida Chaarani <ridachaarani@yahoo.com>
2024-10-18 13:28:26 -0400

    Setup: setup swiper.js and general structor of All Projects

--- porogo/frontend/src/components/views/Projects.tsx
+++ porogo/frontend/src/components/views/Projects.tsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import ProjectCard from '../common/ProjectCard';
+
 
 export default function Projects() {
-    return <h1>Projects</h1>;
+    return <>
+    <section className='test'>
+        <section className='top-content'>
+            <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={50}
+            slidesPerView={1}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+            
+            >
+                <SwiperSlide><ProjectCard name='Important Dog' desc='A discord bot that exists I guess' /></SwiperSlide>
+                <SwiperSlide><ProjectCard name='Portfolio Site' desc='A site to showcase some projects' /></SwiperSlide>
+            </Swiper>
+        </section>
+        </section>
+        <section className='full-content'>
+            
+        </section>
+        </>
+    
 }
--- porogo/frontend/webpack.config.cjs
+++ porogo/frontend/webpack.config.cjs
@@ -20,7 +20,7 @@ module.exports = {
         },
       },
       {
-        test: /\.scss$/,
+        test: /\.(scss|css)$/,
         use: [
           "style-loader",
           "css-loader",
