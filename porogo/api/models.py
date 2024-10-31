b71d3c4bdea32c98c0aa7e47ca7c9d0c869dfefe
Brochua <113867233+Brochua@users.noreply.github.com>
2024-10-31 09:10:48 -0400

    Feat: Create Project model in models.py

--- porogo/api/models.py
+++ porogo/api/models.py
@@ -27,4 +27,3 @@ class Project(models.Model):
     project_status = models.CharField(max_length=1, choices=PROJECT_STATUSES)
     start_year = models.DateField()
     end_year = models.DateField(null=True)
-
