4f6964bb16a84c20eb66009636525bbf9375b0e2
Noah_gelinas <noahgelinas1993@gmail.com>
2024-11-07 16:04:04 -0500

    Begin AboutDev design

--- porogo/frontend/src/components/views/AboutDev.tsx
+++ porogo/frontend/src/components/views/AboutDev.tsx
@@ -1,5 +1,32 @@
 import React from 'react';
+import '../../../static/styles/views/_about-dev.scss';
+import { Link } from 'react-router-dom';
 
 export default function AboutDev() {
-    return <h1>About Dev</h1>;
+    return <>
+        <div id='top-content'>
+            <div className='about'>
+                <h1>Noah Gelinas</h1>
+                <div id="icons-div">
+                    <a href="">
+                        <img id="github" src="/static/assets/icons/github-logo-black.svg" alt="Github Logo" />
+                    </a>
+                    <a href="">
+                        <img id="linkedin" src="/static/assets/icons/linkedin.svg" alt="LinkedIn Logo" />
+                        
+                    </a>
+                    <a href="">
+                        <img id="email" src="/static/assets/icons/email.svg" alt="Email Logo" />
+                    </a>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam accusamus quos quidem veniam sapiente corporis? Quidem porro fuga dolor amet soluta delectus atque aliquam obcaecati, vel consequuntur reprehenderit earum neque.</p>
+            </div> 
+            <div className='about'>
+                <h1>Image</h1>
+            </div>
+        </div> 
+        <div id='full-content'>
+            <h1>About Dev</h1>  
+        </div> 
+    </>;
 }
