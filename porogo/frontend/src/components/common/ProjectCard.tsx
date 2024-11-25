9d0e23f983171c02fcc1783915e61be0561ac887
Axel Brochu <113867233+Brochua@users.noreply.github.com>
2024-11-24 21:10:59 -0500

    Feat: Add Jsdoc eslint (#47)
    * Add eslint config for jsdoc
    
    * Add pre-analysis script to package.json
    
    * Update gitignore file to allow for stats.json
    
    * Add jsdoc comments for ProjectCard component
    
    * Add new frontend ci job for measuring bundle size
    
    * Fix CI action for bundlesize
    
    * Fix CI action for bundlesize
    
    * Run CI without onlyDiff setting
    
    * Remove bundlesize ci

--- porogo/frontend/src/components/common/ProjectCard.tsx
+++ porogo/frontend/src/components/common/ProjectCard.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../../static/styles/common/_project_card.scss';
 
+/**
+ * A card to display short information about a project.
+ * @param props React props
+ * @param props.name Name of the project
+ * @param props.desc Short description of project
+ * @returns An article with information about a project with a link to the project page
+ */
 export default function ProjectCard({ name, desc }: { name: string, desc: string, }) {
     return <article className='project-card'>
         <img src="/static/assets/icons/porogo-logo.svg" alt="placeholder" />
         <div className='project-card-info'>
             <h2>{name}</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero voluptatibus harum velit facilis eligendi. Iusto delectus incidunt quae rem, voluptatum corrupti at autem molestiae, assumenda unde doloribus id totam mollitia.</p>
+            <p>{desc} Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero voluptatibus harum velit facilis eligendi. Iusto delectus incidunt quae rem, voluptatum corrupti at autem molestiae, assumenda unde doloribus id totam mollitia.</p>
             <div className='options'>
                 <Link to={`project/${name}`}>
                     <button>Learn more!</button>
