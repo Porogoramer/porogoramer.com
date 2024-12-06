9016f971aa3af54037d6ba4ad4e1c8df68cdf8c4
Rida Chaarani <ridachaarani@yahoo.com>
2024-12-06 17:37:59 -0500

    Feat: start doiing filter with react-select

--- porogo/frontend/src/components/common/Filter.tsx
+++ porogo/frontend/src/components/common/Filter.tsx
@@ -1,125 +1,27 @@
 import React from 'react';
-import { useMultipleSelection, useSelect } from 'downshift';
 import '../../../static/styles/common/_filter.scss';
+import Select from 'react-select';
 
-function Filter({ elements, label }: { elements: string[], label: string }) {
+function Filter({ elements, label, selectedItems, onChangeHandler }) {
+    const options = elements.map((element) => ({
+        value: element.toLowerCase(),
+        label: element,
+    }));
 
-    function getElementsFilter(selectedItems: string[]) {
-        return function elementsFilter(el: string) {
-            return selectedItems.indexOf(el) < 0;
-        };
-    }
-
-    function MultipleSelect() {
-        const {
-            getSelectedItemProps,
-            getDropdownProps,
-            addSelectedItem,
-            removeSelectedItem,
-            selectedItems,
-        } = useMultipleSelection();
-
-        const items = elements.filter(getElementsFilter(selectedItems));
-
-        const {
-            isOpen,
-            selectedItem,
-            getToggleButtonProps,
-            getLabelProps,
-            getMenuProps,
-            highlightedIndex,
-            getItemProps,
-        } = useSelect({
-            selectedItem: null,
-            defaultHighlightedIndex: 0,
-            items,
-            stateReducer: (state, actionAndChanges) => {
-                const { changes, type } = actionAndChanges;
-                switch (type) {
-                case useSelect.stateChangeTypes.ToggleButtonKeyDownEnter:
-                case useSelect.stateChangeTypes.ToggleButtonKeyDownSpaceButton:
-                case useSelect.stateChangeTypes.ItemClick:
-                    return {
-                        ...changes,
-                        isOpen: true,
-                        highlightedIndex: 0,
-                    };
-                }
-                return changes;
-            },
-            onStateChange: ({ type, selectedItem: newSelectedItem }) => {
-                switch (type) {
-                case useSelect.stateChangeTypes.ToggleButtonKeyDownEnter:
-                case useSelect.stateChangeTypes.ToggleButtonKeyDownSpaceButton:
-                case useSelect.stateChangeTypes.ItemClick:
-                case useSelect.stateChangeTypes.ToggleButtonBlur:
-                    if (newSelectedItem) {
-                        addSelectedItem(newSelectedItem);
-                    }
-                    break;
-                default:
-                    break;
-                }
-            },
-        });
-        return (
-            <div className="multiple-select-container">
-                <div className="multiple-select">
-                    <label className="multiple-select-label" {...getLabelProps()}>
-                Pick some books:
-                    </label>
-                    <div className="selected-items-container">
-                        {selectedItems.map(function renderSelectedItem(selectedItemForRender, index) {
-                            return (
-                                <span
-                                    className="selected-item"
-                                    key={`selected-item-${index}`}
-                                    {...getSelectedItemProps({ selectedItem: selectedItemForRender, index })}
-                                >
-                                    {selectedItemForRender}
-                                    <span
-                                        className="remove-item"
-                                        onClick={e => {
-                                            e.stopPropagation();
-                                            removeSelectedItem(selectedItemForRender);
-                                        }}
-                                    >
-                        &#10005;
-                                    </span>
-                                </span>
-                            );
-                        })}
-                        <div
-                            className="dropdown-toggle"
-                            {...getToggleButtonProps(getDropdownProps({ preventKeyAction: isOpen }))}
-                        >
-                            {label} &#8595;
-                        </div>
-                    </div>
-                </div>
-    
-                <ul
-                    className={`dropdown-menu ${!(isOpen && items.length) && 'hidden'}`}
-                    {...getMenuProps()}
-                >
-                    {isOpen &&
-                items.map((item, index) => (
-                    <li
-                        className={`${
-                            highlightedIndex === index ? 'highlighted' : ''
-                        } ${selectedItem === item ? 'selected-item-bold' : ''} dropdown-item`}
-                        key={`${item}${index}`}
-                        {...getItemProps({ item, index })}
-                    >
-                        <span>{item}</span>
-                    </li>
-                ))}
-                </ul>
-            </div>
-        );
-    }
-
-    return <MultipleSelect />;
+    return (
+        <div className="filter-container">
+            {label && <label className="filter-label">{label}</label>}
+            <Select
+                isMulti
+                name="filters"
+                options={options}
+                value={selectedItems}
+                onChange={onChangeHandler}
+                className="basic-multi-select"
+                classNamePrefix="select"
+            />
+        </div>
+    );
 }
 
 export default Filter;
--- porogo/frontend/src/components/views/Projects.tsx
+++ porogo/frontend/src/components/views/Projects.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Swiper, SwiperSlide} from 'swiper/react';
+import React, { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, A11y, Autoplay, EffectFade } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/bundle';
@@ -8,54 +8,97 @@ import Card from '../common/Card';
 import Filter from '../common/Filter';
 import '../../../static/styles/views/_allprojects.scss';
 
-
-
 export default function Projects() {
+    const allCards = [
+        { id: 1, languages: ['JS'], authors: ['Axel'] },
+        { id: 2, languages: ['JAVA'], authors: ['Noah'] },
+        { id: 3, languages: ['PYTHON'], authors: ['Spryte'] },
+        { id: 4, languages: ['C++'], authors: ['Soup'] },
+        { id: 5, languages: ['F#', 'JS'], authors: ['Rida'] },
+    ];
+
     const languages = ['JS', 'JAVA', 'PYTHON', 'C++', 'C#', 'HTML', 'CSS', 'Kotlin'];
-    const languages2 = ['JS', 'JAVA', 'PYTHON'];
-    const names = ['Rida', 'Axel', 'Noah', 'Spryte', 'Soup'];
-    return <>
-        <section className='test'>
-            <section className='top-content'>
-                <Swiper className='carousel'
-                    modules={[Navigation, Pagination, A11y, Autoplay, EffectFade]}
-                    spaceBetween={50}
-                    slidesPerView={1}
-                    loop={true}
-                    autoplay={{
-                        delay: 5000,
-                        disableOnInteraction: true,
-                    }}
-                    pagination = {true}
-                    effect='fade'>
-                    <SwiperSlide className='card'><ProjectShowcase name='Important Dog' desc='A discord bot that exists I guess' date='2004-2255' backgroundImage='/static/assets/images/placeholder1.jpg'/></SwiperSlide>
-                    <SwiperSlide className='card'><ProjectShowcase name='Portfolio Site' desc='A site to showcase some projects' date='2999-0999' backgroundImage='/static/assets/images/placeholder2.jpg'/></SwiperSlide>
-                </Swiper>
+    const names = ['Axel', 'Noah', 'Spryte', 'Soup', 'Rida'];
+
+    const [selectedAuthors, setSelectedAuthors] = useState([]);
+    const [selectedLanguages, setSelectedLanguages] = useState([]);
+
+    const filteredCards = allCards.filter(
+        (card) =>
+            (selectedAuthors.length === 0 || selectedAuthors.includes(card.authors[0].toLowerCase())) &&
+            (selectedLanguages.length === 0 || card.languages.some((lang) => selectedLanguages.includes(lang.toLowerCase())))
+    );
+
+    function changeAuthorHandler(authors) {
+        setSelectedAuthors(authors);
+    }
+
+    function changeLanguageHandler(langs) {
+        setSelectedLanguages(langs);
+    }
+
+
+    return (
+        <>
+            <section className="test">
+                <section className="top-content">
+                    <Swiper
+                        className="carousel"
+                        modules={[Navigation, Pagination, A11y, Autoplay, EffectFade]}
+                        spaceBetween={50}
+                        slidesPerView={1}
+                        loop={true}
+                        autoplay={{
+                            delay: 5000,
+                            disableOnInteraction: true,
+                        }}
+                        pagination={true}
+                        effect="fade"
+                    >
+                        <SwiperSlide className="card">
+                            <ProjectShowcase
+                                name="Important Dog"
+                                desc="A discord bot that exists I guess"
+                                date="2004-2255"
+                                backgroundImage="/static/assets/images/placeholder1.jpg"
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide className="card">
+                            <ProjectShowcase
+                                name="Portfolio Site"
+                                desc="A site to showcase some projects"
+                                date="2999-0999"
+                                backgroundImage="/static/assets/images/placeholder2.jpg"
+                            />
+                        </SwiperSlide>
+                    </Swiper>
+                </section>
             </section>
-        </section>
-        <section className='bottom-content'>
-            <div id='filter'>
-                <Filter elements={names} label="Authors" />
-                <Filter elements={languages} label="Languages" />
-            </div>
-            <div id='projects'>
-                <h1>PROJECTS</h1>
-                <div className='allCards'>
-                    <Card languages={languages} authors={names}/>
-                    <Card languages={languages} authors={names}/>
-                    <Card languages={languages} authors={names}/>
-                    <Card languages={languages} authors={names}/>
-                    <Card languages={languages} authors={names}/>
-                    <Card languages={languages} authors={names}/>
-                    <Card languages={languages} authors={names}/>
-                    <Card languages={languages} authors={names}/>
-                    <Card languages={languages} authors={names}/>
-                    <Card languages={languages} authors={names}/>
-                    <Card languages={languages} authors={names}/>
-                    <Card languages={languages} authors={names}/>
+            <section className="bottom-content">
+                <div id="filter">
+                    <Filter
+                        elements={names}
+                        label="Authors"
+                        selectedItems={selectedAuthors}
+                        onChangeHandler={changeAuthorHandler}
+                    />
+                    <Filter
+                        elements={languages}
+                        label="Languages"
+                        selectedItems={selectedLanguages}
+                        onChangeHandler={changeLanguageHandler}
+                    />
 
                 </div>
-            </div>           
-        </section>
-    </>;
+                <div id="projects">
+                    <h1>PROJECTS</h1>
+                    <div className="allCards">
+                        {filteredCards.map((card) => (
+                            <Card key={card.id} languages={card.languages} authors={card.authors} />
+                        ))}
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 }
