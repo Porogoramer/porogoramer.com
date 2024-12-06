b3d92f7f17392a585392c26cc0ba81566906f7ae
Brochua <113867233+Brochua@users.noreply.github.com>
2024-12-06 11:17:13 -0500

    Feat: Set cache control header for index.html

--- porogo/frontend/views.py
+++ porogo/frontend/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     '''View that renders the react app through the index.html file'''
-    return render(request, 'frontend/index.html')
+    response = render(request, 'frontend/index.html')
+    response.headers['Cache-Control'] = 'no-store'
+    return response
