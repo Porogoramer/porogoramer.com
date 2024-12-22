908fc9fc224ccc35e4bfa04fd1e27c8fbf06067e
Yan2arb4 <yan2arb6@hotmail.com>
2024-12-22 15:19:18 -0500

    setup : add more font weight options

--- porogo/frontend/src/components/views/About.tsx
+++ porogo/frontend/src/components/views/About.tsx
@@ -7,17 +7,14 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-/**
- *
- */
 export default function About() {
     const people = ['Yaneric', 'Axel', 'Rida', 'Emilie', 'Noah'];
     return <>
         <section className='intro'>
             <div className='text'>
-                <p>ABOUT US</p>
-                <p>Porogo crafts innovative and meaningful and fun projects.</p>
-                <p>
+                <h4 className='subtitle'>ABOUT US</h4>
+                <h1 className='title'>Porogo crafts innovative and meaningful and fun projects.</h1>
+                <p className='description'>
                         We came up with Porogo as a way to bring a team of passionate creators together to build
                         projects we truly enjoy. We believe the best work comes from loving what we do, and we 
                         channel that enthusiasm into crafting innovative and meaningful projects. At Porogo, we 
@@ -31,9 +28,9 @@ export default function About() {
         </section>
         <section className='team'>
             <div className='text'>
-                <p>OUR TEAM</p>
-                <p>Our team is a passionate group dedicated to bringing ideas to life.</p>
-                <p>
+                <h4 className='subtitle'>OUR TEAM</h4>
+                <h1 className='title'>Our team is a passionate group dedicated to bringing ideas to life.</h1>
+                <p className='description'>
                         At Porogo, we&apos;re a team of &apos;Porogoramers&apos; who turn our passions into impactful projects. Whether 
                         we&apos;re playing Minecraft and building a server hosting platform to share with others, or diving into 
                         D&D and creating apps that enhance the player experience, we&apos;re all about crafting tools that make 
@@ -58,24 +55,26 @@ export default function About() {
             </div>
         </section>
         <section className='people'>
-            <p>THE PEOPLE</p>
-            <p>Here is everyone part of the team</p>
+            <h4 className='subtitle'>THE PEOPLE</h4>
+            <h1>Here is everyone part of the team</h1>
             {/*API get porogoramers and fetch github pfp*/}
             <div className='people-list'>
                 {people.map(person => (
                     <div className='person' key={person}>
                         <p>{person}</p>
                         <img src='/static/assets/images/github-pfp.png'></img>
+                        <p>developer</p>
                     </div>
                 ))}
             </div>
         </section>
         <section className='projects'>
-            <p>PROJECTS</p>
-            <p>Things we&apos;ve made</p>
+            <h4 className='subtitle'>PROJECTS</h4>
+            <h1>Things we&apos;ve made</h1>
             {/*API get Github projects with most stars from organization (only need 2)*/}
             <div className='star-projects'>
-
+                <p>card project</p>
+                <p>card project</p>
                 <div className='more'>
                     <p>Checkout the rest !</p>
                     <img src='/static/assets/icons/three-circles.svg'></img>
