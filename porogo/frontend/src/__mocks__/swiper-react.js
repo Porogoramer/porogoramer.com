1546704cee9850b48529963e7773ee554f251b2e
Rida <ridachaarani@yahoo.com>
2025-01-05 00:40:54 -0500

    Fix: Remove temp code to merge

--- porogo/frontend/src/__mocks__/swiper-react.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    Swiper: ({ children }) => <div data-testid="swiper">{children}</div>,
-    SwiperSlide: ({ children }) => <div data-testid="swiper-slide">{children}</div>,
-};
