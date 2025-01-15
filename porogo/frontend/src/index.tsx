4c0954a646882ba45361653ac694dfcfc5aeb0fd
Axel Brochu <113867233+Brochua@users.noreply.github.com>
2025-01-15 18:53:13 -0500

    Frontend/home mobile (#57)
    * Style: Update home page to be responsive for phone layout
    
    * Style: Update footer to be responsive for phone layout

--- porogo/frontend/src/index.tsx
+++ porogo/frontend/src/index.tsx
@@ -26,11 +26,11 @@ const root = createRoot(appDiv);
  */
 function Main() {
     return(
-        <div>
+        <>
             <Header/>
             <Outlet/>
             <Footer/>
-        </div>
+        </>
     );
 }
 
