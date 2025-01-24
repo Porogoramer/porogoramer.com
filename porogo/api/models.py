c232efd2ca5787e5e9483322e2634f4bd970cdbc
Brochua <113867233+Brochua@users.noreply.github.com>
2025-01-24 15:47:49 -0500

    Fix: update unit tests for backend

--- porogo/api/models.py
+++ porogo/api/models.py
@@ -90,8 +90,8 @@ class Developer(models.Model):
     other_url = models.URLField(max_length=200, null=True, blank=True)
     other_url_type = models.CharField(max_length=20, null=True, blank=True)
     picture = models.ForeignKey(Image, on_delete=models.CASCADE, related_name="developer_picture")
-    picture_arms = models.ForeignKey(Image, on_delete=models.CASCADE, related_name="developer_arms_picture")
-    picture_personal = models.ForeignKey(Image, on_delete=models.CASCADE, related_name="developer_personal_picture")
+    picture_arms = models.ForeignKey(Image, on_delete=models.CASCADE, related_name="developer_arms_picture", null=True)
+    picture_personal = models.ForeignKey(Image, on_delete=models.CASCADE, related_name="developer_personal_picture", null=True)
     languages = models.ManyToManyField(Language)
     models.UniqueConstraint(fields=["first_name", "last_name"], name="unique_full_name")
 
